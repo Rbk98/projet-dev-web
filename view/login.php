1390e453a68b88b9a825cd019d34927be390fd75
anais-11 <adegorre@ensc.fr>
2022-04-26 16:07:04 +0200

    modifs forms

--- view/login.php
+++ view/login.php
@@ -2,33 +2,35 @@
 $title = 'Inscription';
 ob_start();
 ?>
-    <div class="container my-2">
-        <div class="container d-flex justify-content-center m-5">
-            <form>
-                <fieldset class="bg-light p-4">
-                    <legend>Connexion</legend>
-                    <div class="row mb-5">
-                        <div class="col-12 mb-3">
-                            <label for="nickname" class="col-form-label">Surnom :</label>
-                            <input type="text" class="form-control" id="nickname" name="nickname"
-                                   placeholder="Entrez votre surnom" required>
+    <div class="container my-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-sm-12 col-lg-5">
+                <form>
+                    <fieldset class="form bg-light p-4">
+                        <legend class="text-center">Connexion</legend>
+                        <div class="row mb-5">
+                            <div class="col-12 mb-3">
+                                <label for="nickname" class="col-form-label">Surnom :</label>
+                                <input type="text" class="form-control" id="nickname" name="nickname"
+                                    placeholder="Entrez votre surnom" required>
+                            </div>
+                            <div class="col-12">
+                                <label for="password" class="col-form-label">Mot de passe :</label>
+                                <input type="password" class="form-control" id="password" name="password"
+                                    placeholder="Entrez votre mot de passe" required>
+                            </div>
                         </div>
-                        <div class="col-12">
-                            <label for="password" class="col-form-label">Mot de passe :</label>
-                            <input type="password" class="form-control" id="password" name="password"
-                                   placeholder="Entrez votre mot de passe" required>
+                        <div class="row">
+                            <div class="col-12 d-grid gap-2 mb-3">
+                                <button type="sumbit" class="btn btn-success ">Se connecter</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 d-grid gap-2">
-                            <button type="sumbit" class="btn btn-success ">Se connecter</button>
+                        <div class="col-12 d-flex justify-content-center">
+                            <p class="login_register">Pas encore de compte ? <a class="text-success text-decoration-none" href="registration.php">Je m'inscris</a></p>
                         </div>
-                    </div>
-                    <div class="col-12 d-flex justify-content-center">
-                        <p>Pas encore de compte ? <a href="registration.php">Je m'inscris</a></p>
-                    </div>
-                </fieldset>
-            </form>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     </div>
 <?php $content = ob_get_clean();
