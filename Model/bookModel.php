edb5b9ce058bcc12ee13e573a04cab9a4a6d21f6
Margob29 <mbiret@ensc.fr>
2022-04-30 17:26:17 +0200

    backend my creations

--- Model/bookModel.php
+++ Model/bookModel.php
@@ -4,17 +4,35 @@ require('connect.php');
 function getBestBooks()
 {
     $bdd = connectDb();
-    $books = $bdd->query('SELECT * FROM Book ORDER BY nb_reading DESC LIMIT 4');
+    $books = $bdd->query('SELECT * FROM cover ORDER BY nb_win DESC LIMIT 4');
 
     return $books;
 }
 
 function getBook($idBook){
     $bdd = connectDb();
-    $book = $bdd->prepare("SELECT * FROM Book WHERE id_book =?");
+    $book = $bdd->prepare("SELECT * FROM cover WHERE id_book =?");
     $book->execute(array($idBook));
     if ($book->rowCount() == 1)
         return $book->fetch();  // Accès à la première ligne de résultat
     else
         throw new Exception("Aucun livre ne correspond à l'identifiant '$idBook'");
+}
+
+function getStartedCreation()
+{
+    $bdd = connectDb();
+    $books = $bdd->prepare('SELECT * FROM cover WHERE writter=? AND state=1');
+    $books->execute(array($_SESSION['id_user']));
+
+    return $books;
+}
+
+function getFinishedCreation()
+{
+    $bdd = connectDb();
+    $books = $bdd->prepare('SELECT * FROM cover WHERE writter=:writer AND (state=2 OR state=3)');
+    $books->execute(array("writer"=>$_SESSION['id_user']));
+
+    return $books;
 }
--- view/my_creations.php
+++ view/my_creations.php
@@ -1,106 +1,119 @@
-<?php $title = 'Mes creations'; ?>
+<?php $title = 'Mes creations';
+    require_once('./Model/bookModel.php');
+    ob_start();
+?>
 
-<?php ob_start(); ?>
     <div class="container my-2">
         <h2 class="h2_title my-2">Mes créations</h2>
         <hr class="hr_content"/>
     </div>
 
-
-
-<div class="container my-5"> 
-
-
-    <div class="row">
-        <div class="col d-flex justify-content-center">
-            <div class="card text-center" style="width: 30rem;">                
-                <div class="card-body">
-                     <i class="fa fa-book fa-2x mb-4" style="color:#0883cd;"></i>
-                    <p class="mb-4">Vous n'avez pour l'instant aucune histoire de créée. </br>Commencez à écrire votre première histoire dès maintenant </p>
-                    <a type="sumbit" class="btn btn-success" href="create_story.php">Créer mon histoire</a>
-                </div>
-            </div>
-        </div>   
-    </div>
-</div>
-    <div class="bg_crea container mb-3">
-        <h4>Histoire(s) en cours de création</h4>
-    </div>
-    <div class="row row-cols-1 row-cols-md-4 g-4 mx-4">
-        <div class="col-2">
-            <div class="card h-75">
-                <img src="public/images/book_1.jpg" class="card-img-top py-3" alt="book_img">
-                <div class="card-body">
-                    <h5 class="card-title">Histoire n°1</h5>
-                    <a href="index.php?action=modifier-histoire" class="btn btn-primary px-2">Continuer
-                        l'édition</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="card h-75">
-                <img src="public/images/book_1.jpg" class="card-img-top py-3" alt="book_img">
-                <div class="card-body">
-                    <h5 class="card-title">Histoire n°2</h5>
-                    <a href="index.php?action=modifier-histoire" class="btn btn-primary px-2">Continuer
-                        l'édition</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="card h-75">
-                <img src="public/images/book_1.jpg" class="card-img-top py-3" alt="book_img">
-                <div class="card-body">
-                    <h5 class="card-title">Histoire n°3</h5>
-                    <a href="index.php?action=modifier-histoire" class="btn btn-primary px-2">Continuer
-                        l'édition</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="card h-75">
-                
-                <div class="card-body">
-                    <a href="index.php?action=creer-histoire"><i class="bi bi-plus-circle" style="font-size:100px; "></i></a>
-                    <h6 class="card-title mt-5 d-flex justify-content-center">Créer une nouvelle histoire</h6>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="mb-2">
-        <hr></hr>
-    </div>
-    <div class="bg_crea container m-3">
-        <h4>Histoires terminées</h4>
-    </div>
-    <div class="row row-cols-1 row-cols-md-4 g-4 mx-4">
-            <div class="col-3 mb-5">
-                <div class="card h-100">
-                    <img src="public/images/book_1.jpg" class="card-img-top p-3" alt="book_img">
-                    <div class="card-body">
-                        <h5 class="card-title">Histoire n°1</h5>
-                        <span class="badge bg-info text-dark">Aventure</span>
-                        <p class="card-text">Petit résumé concernant la première histoire...</p>
-                        <a href="index.php?action=mon-histoire&1" class="btn btn-success px-2 mb-2">
-                            <i class="bi bi-eye"></i> Publié</a>
-                        <a href="index.php?action=info-histoire" class="btn btn-light px-2">
-                            <i class="bi bi-graph-up"></i> Voir mes statistiques</a>
+<?php if(isset($_SESSION['nickname']) && isset($_SESSION['role']))
+{
+    if(!empty($_SESSION['nickname']) && !empty($_SESSION['role']))
+    {   
+        if($_SESSION['role'] ==1)
+        {
+            //s'il n'y a pas encore d'histoire créée
+            if(getStartedCreation()->rowCount()==0 && getFinishedCreation()->rowCount()==0)
+            {?>
+                <div class="container my-5"> 
+                    <div class="row">
+                        <div class="col d-flex justify-content-center">
+                            <div class="card text-center" style="width: 30rem;">                
+                                <div class="card-body">
+                                    <i class="fa fa-book fa-2x mb-4" style="color:#0883cd;"></i>
+                                    <p class="mb-4">Vous n'avez pour l'instant aucune histoire de créée. </br>Commencez à écrire votre première histoire dès maintenant </p>
+                                    <a type="sumbit" class="btn btn-success" href="create_story.php">Créer mon histoire</a>
+                                </div>
+                            </div>
+                        </div>   
                     </div>
                 </div>
-            </div>
-            <div class="col-3 mb-5">
-                <div class="card h-100">
-                    <img src="public/images/book_1.jpg" class="card-img-top p-3" alt="book_img">
-                    <div class="card-body">
-                        <h5 class="card-title">Histoire n°2</h5>
-                        <span class="badge bg-info text-dark">Aventure</span>
-                        <p class="card-text">Petit résumé concernant la première histoire...</p>
-                        <a href="index.php?action=info-histoire" class="btn btn-secondary px-2">
-                            <i class="bi bi-eye-slash"></i> Non publié</a>
+            <?php }
+            else {
+                //s'il y a des histoires en cours de création
+                if(getStartedCreation()->rowCount()!=0)
+                { ?>
+                    <div class="container mb-3">
+                        <h4>Histoire(s) en cours de création</h4>
                     </div>
-                </div>
-            </div>
-    </div>
+                    <?php
+                    $result=getStartedCreation();
+                    $result=$result->fetchAll();
+                    foreach($result as $ligne)
+                    { ?>
+                        <div class="row row-cols-1 row-cols-md-4 g-4 mx-4">
+                            <div class="col-2">
+                                <div class="card h-75">
+                                    <img src="public/images/<?= $ligne['image']?>" class="card-img-top py-3" alt="book_img">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?=$result['title']?></h5>
+                                        <a href="index.php?action=modifier-histoire" class="btn btn-primary px-2">Continuer
+                                            l'édition</a>
+                                    </div>
+                                </div>
+                            </div>  
+                        </div>
+                        <div class="col-2">
+                            <div class="card h-75">
+                                 <div class="card-body">
+                                    <a href="index.php?action=creer-histoire"><i class="bi bi-plus-circle" style="font-size:100px; "></i></a>
+                                    <h6 class="card-title mt-5 d-flex justify-content-center">Créer une nouvelle histoire</h6>
+                                </div>
+                            </div>
+                        </div>
+                    <?php }
+                }
+                //s'il y a des histoires en cours de création ET des histoires finies
+                if(getFinishedCreation()->rowCount()!=0 && getStartedCreation()->rowCount()!=0)
+                { ?>
+                    <div class="mb-2 mx-2">
+                        <hr></hr>
+                    </div>
+                <?php }
+                //s'il y a des histoires finies
+                if (getFinishedCreation()->rowCount()!=0)
+                { ?>
+                    <div class="container m-3">
+                        <h4>Histoires terminées</h4>
+                    </div>
+                    <?php
+                    $result=getFinishedCreation();
+                    $result=$result->fetchAll();
+                    foreach($result as $ligne)
+                    { ?>
+                        <div class="row row-cols-1 row-cols-md-4 g-4 mx-4">
+                            <div class="col-3 mb-5">
+                                <div class="card h-100">
+                                    <img src="public/images/<?= $ligne['image']?>" class="card-img-top p-3" alt="book_img">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?=$result['title']?></h5>
+                                        <span class="badge bg-info text-dark"><?=$result['genre']?></span>
+                                        <p class="card-text"><?=$result['summary']?></p>
+                                        <?php
+                                        if($result['state']==2)
+                                        { ?>
+                                            <a href="index.php?action=mon-histoire&1" class="btn btn-success px-2 mb-2">
+                                                <i class="bi bi-eye"></i> Publié</a>
+                                            <a href="index.php?action=info-histoire" class="btn btn-light px-2">
+                                                <i class="bi bi-graph-up"></i> Voir mes statistiques</a>
+                                        <?php }
+                                        else if ($result['state']==3){?>
+                                            <a href="index.php?action=info-histoire" class="btn btn-secondary px-2">
+                                            <i class="bi bi-eye-slash"></i> Non publié</a>
+                                        <?php } ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php }
+                }
+            }
+        }
+    }
+}
 
-<?php $content = ob_get_clean();
-require('base.php'); ?>
+
+$content = ob_get_clean();
+require('base.php');?>
