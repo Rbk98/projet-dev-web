71a8ffe05768e4ec046fe37bd53f5f42710525f0
Rebecca Grenet <rebeccagnt@orange.fr>
2022-05-02 09:40:09 +0200

    add database

--- DB/bookproject.sql
+++ DB/bookproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3308
--- Généré le : ven. 29 avr. 2022 à 16:27
+-- Généré le : lun. 02 mai 2022 à 07:39
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.4.9
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `bookproject`
 --
-CREATE DATABASE IF NOT EXISTS `bookproject` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `bookproject`;
 
 -- --------------------------------------------------------
 
@@ -29,6 +27,7 @@ USE `bookproject`;
 -- Structure de la table `choice`
 --
 
+DROP TABLE IF EXISTS `choice`;
 CREATE TABLE IF NOT EXISTS `choice` (
   `id_choice` int(10) NOT NULL,
   `id_next_story` int(10) NOT NULL,
@@ -43,19 +42,29 @@ CREATE TABLE IF NOT EXISTS `choice` (
 -- Structure de la table `cover`
 --
 
+DROP TABLE IF EXISTS `cover`;
 CREATE TABLE IF NOT EXISTS `cover` (
   `id_book` int(5) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `summary` text NOT NULL,
   `genre` varchar(255) NOT NULL,
-  `writter` int(10) NOT NULL,
+  `writer` int(10) NOT NULL,
   `date` date NOT NULL,
   `nb_lives` int(10) NOT NULL,
   `nb_stories_max` int(10) NOT NULL,
   `nb_win` int(10) NOT NULL,
+  `nb_reading` int(10) NOT NULL DEFAULT '0',
   `image` varchar(255) DEFAULT NULL,
+  `status` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_book`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `cover`
+--
+
+INSERT INTO `cover` (`id_book`, `title`, `summary`, `genre`, `writer`, `date`, `nb_lives`, `nb_stories_max`, `nb_win`, `nb_reading`, `image`, `status`) VALUES
+(1, 'Tom Sawyer', 'Thomas Sawyer est un orphelin d\'âge indéterminé, qui ne pense qu\'à faire l\'école buissonnière, à s\'identifier aux personnages de ses romans préférés et à jouer des tours à ses camarades. ', 'Aventure', 1, '2022-04-03', 3, 4, 2, 0, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -63,6 +72,7 @@ CREATE TABLE IF NOT EXISTS `cover` (
 -- Structure de la table `story`
 --
 
+DROP TABLE IF EXISTS `story`;
 CREATE TABLE IF NOT EXISTS `story` (
   `id_story` int(10) NOT NULL,
   `id_cover` int(10) NOT NULL,
@@ -77,15 +87,26 @@ CREATE TABLE IF NOT EXISTS `story` (
 -- Structure de la table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(10) NOT NULL AUTO_INCREMENT,
   `nickname` varchar(255) NOT NULL,
   `birth` date NOT NULL,
   `password` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
   `role` int(5) DEFAULT '0',
   `nb_reading` int(10) DEFAULT '0',
-  PRIMARY KEY (`nickname`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_user`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nickname`, `birth`, `password`, `mail`, `role`, `nb_reading`) VALUES
+(1, 'rbk98', '1998-10-28', '$2y$10$tq.TY6DNqZAQ2G.o.XK/C.IyaOjkNR0NSkrveTy9YB9X84AJvKMHS', 'rebeccagnt@orange.fr', 0, 0),
+(2, 'FKERL', '4444-04-24', '$2y$10$nq4h.teIswHaZhVf82HDIOHKAMOfBwmbAiZJrTg2bezROqDFUiQo.', 'rebeccagrenet1@gmail.com', 0, 0),
+(3, 'rbk', '9321-12-29', '$2y$10$msJrtNDwH8Ry6RJ0009CLe7eiHyBPtdPKp9jNk6kbxu9wSNEIUbe2', 'rebeccagrenet1@gmail.com', 0, 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
