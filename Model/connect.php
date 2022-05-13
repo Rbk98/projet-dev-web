faa4db3e5523ec050844d4e32323762efc5c8a17
Rebecca Grenet <rebeccagnt@orange.fr>
2022-05-13 13:01:17 +0200

    update

--- Model/connect.php
+++ Model/connect.php
@@ -2,18 +2,18 @@
 
 function connectDb()
 {
-    /*Local deployment*/
+    /*Local deployment
     $server = "localhost";
     $username = "projetS2";
     $password = "amr2022";
-    $db = "bookproject";
+    $db = "bookproject";*/
 
-    /*ZZZ deployment
+    /*ZZZ deployment*/
     $server = "localhost";
     $username = "rgrenet";
     $password = "22BdxS2!rbk";
     $db = "rgrenet";
-    */
+
     $bdd = new PDO(
         'mysql:host=' . $server . ':3308;dbname=' . $db . ';charset=utf8',
         $username,
